6b93e45ab4fb18d4093c1e946bdc2a9ab13b5d30
Wim Looman <ghostunderscore@gmail.com>
2015-02-05 22:48:57 +1300

    Double Key??

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,7 @@
 use glfw::{ Key, WindowEvent };
 use glfw::Action::{ Press, Release };
 use glfw::WindowEvent::Close;
-use glfw::WindowEvent::Key as WKey;
+use glfw::WindowEvent::Key;
 
 use camera::{ Action, Direction };
 use camera::Action::{ Start, Stop };
@@ -17,20 +17,20 @@ pub enum Event {
 impl Event {
     pub fn from_glfw(event: WindowEvent) -> Option<Event> {
         match event {
-            Close                            => Some(Quit),
-            WKey(Key::Escape, _, Press,   _) => Some(Quit),
-            WKey(Key::A,      _, Press,   _) => Some(Camera(Left, Start)),
-            WKey(Key::A,      _, Release, _) => Some(Camera(Left, Stop)),
-            WKey(Key::W,      _, Press,   _) => Some(Camera(Forward, Start)),
-            WKey(Key::W,      _, Release, _) => Some(Camera(Forward, Stop)),
-            WKey(Key::D,      _, Press,   _) => Some(Camera(Right, Start)),
-            WKey(Key::D,      _, Release, _) => Some(Camera(Right, Stop)),
-            WKey(Key::S,      _, Press,   _) => Some(Camera(Back, Start)),
-            WKey(Key::S,      _, Release, _) => Some(Camera(Back, Stop)),
-            WKey(Key::Space,  _, Press,   _) => Some(Camera(Up, Start)),
-            WKey(Key::Space,  _, Release, _) => Some(Camera(Up, Stop)),
-            WKey(Key::Z,      _, Press,   _) => Some(Camera(Down, Start)),
-            WKey(Key::Z,      _, Release, _) => Some(Camera(Down, Stop)),
+            Close                           => Some(Quit),
+            Key(Key::Escape, _, Press,   _) => Some(Quit),
+            Key(Key::A,      _, Press,   _) => Some(Camera(Left, Start)),
+            Key(Key::A,      _, Release, _) => Some(Camera(Left, Stop)),
+            Key(Key::W,      _, Press,   _) => Some(Camera(Forward, Start)),
+            Key(Key::W,      _, Release, _) => Some(Camera(Forward, Stop)),
+            Key(Key::D,      _, Press,   _) => Some(Camera(Right, Start)),
+            Key(Key::D,      _, Release, _) => Some(Camera(Right, Stop)),
+            Key(Key::S,      _, Press,   _) => Some(Camera(Back, Start)),
+            Key(Key::S,      _, Release, _) => Some(Camera(Back, Stop)),
+            Key(Key::Space,  _, Press,   _) => Some(Camera(Up, Start)),
+            Key(Key::Space,  _, Release, _) => Some(Camera(Up, Stop)),
+            Key(Key::Z,      _, Press,   _) => Some(Camera(Down, Start)),
+            Key(Key::Z,      _, Release, _) => Some(Camera(Down, Stop)),
             _ => None,
         }
     }
